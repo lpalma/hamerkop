cf8ddd7ef7b2434bc4ce6a464ccb8580cdf9b0d5
Luciano Palma <lucianopalma90@gmail.com>
2017-04-15 18:13:11 +0100

    create basic helper

---
+++ test/Helper.hs
@@ -0,0 +1,19 @@
+module Helper where
+
+import Types
+import qualified Data.Map.Lazy as Map
+import Data.Time.Calendar (fromGregorian)
+import Data.Time.Clock
+import Env
+
+emptyEnv :: Env
+emptyEnv = ([], Map.empty, midnight)
+
+newUserAct :: String -> String -> String -> UTCTime
+newUserAct n act args t = UserAct { userName = n
+                                  , action = act
+                                  , args = args
+                                  , time = t }
+
+midnight :: UTCTime
+midnight = UTCTime (fromGregorian 2017 1 1) 0
