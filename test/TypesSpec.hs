e6d8cdc9c6948ccf5f6c39550849c022cc44dbaa
Luciano Palma <lucianopalma90@gmail.com>
2017-04-16 20:38:53 +0100

    add tests for Types module

---
+++ test/TypesSpec.hs
@@ -0,0 +1,18 @@
+module TypesSpec (spec) where
+
+import Helper
+import Test.Hspec
+import Types
+
+spec :: Spec
+spec = do
+  describe "Types.toUserAction" $ do
+
+    it "should return a UserAction with all values set" $ do
+      toUserAction ["a", "->", "b"] midnight `shouldBe` newUserAct "a" "->" "b" midnight
+
+    it "should return a UserAction with username and time only" $ do
+      toUserAction ["a"] midnight `shouldBe` newUserAct "a" "" "" midnight
+
+    it "should return a UserAction without args" $ do
+      toUserAction ["a", "->"] midnight `shouldBe` newUserAct "a" "->" "" midnight
