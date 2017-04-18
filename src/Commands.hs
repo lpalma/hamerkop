7b7b3d3c73d5f21d032835b37c46cba11cca7526
Luciano Palma <lucianopalma90@gmail.com>
2017-04-18 09:10:39 +0100

    add func for reading posts

--- src/Commands.hs
+++ src/Commands.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Commands (postRunner) where
+module Commands
+  ( postRunner
+  , readingRunner
+  ) where
 
 import Control.Monad.State.Strict
-import qualified Data.Map.Lazy as Map (alter)
+import qualified Data.Map.Lazy as Map (alter, lookup)
+import Data.DateTime (diffMinutes, diffSeconds)
 import Data.Time.Clock (UTCTime)
 import Types
 
@@ -11,6 +15,26 @@ postRunner :: ActionRunner
 postRunner a@UserAct{..} = do
   modify $ upsert a
   return $ userName ++ ": " ++ args
+
+readingRunner :: ActionRunner
+readingRunner a@UserAct{..} = gets $ unlines . userPosts userName
+
+userPosts :: String -> Env -> [String]
+userPosts name e@Env{..} =
+  case Map.lookup name users of
+    Nothing -> [name ++ " hasn't posted anything yet."]
+    Just User{..} -> map (formatPost eTime) posts
+
+formatPost :: UTCTime -> Post -> String
+formatPost time Post{..} = msg ++ " (" ++ (formatTime date time) ++ ")"
+
+formatTime :: UTCTime -> UTCTime -> String
+formatTime post env
+  | diff <= 1 = "just now"
+  | diff < 60 = show diff ++ " seconds ago"
+  | diff <= 119 = "1 minute ago"
+  | otherwise = show (diffMinutes post env) ++ " minutes ago"
+  where diff = diffSeconds post env
 
 upsert :: Action -> Env -> Env
 upsert a@UserAct{..} e@Env{..} = e { users = Map.alter upsert' userName users }
