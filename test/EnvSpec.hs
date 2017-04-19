44d3a3be64560df0fb264010b7e2a67ed39d3ef8
Luciano Palma <lucianopalma90@gmail.com>
2017-04-19 11:08:05 +0100

    add tests for findUsers

--- test/EnvSpec.hs
+++ test/EnvSpec.hs
@@ -1,14 +1,14 @@
 module EnvSpec (spec) where
 
 import Control.Monad.State.Strict
-import qualified Data.Map.Lazy as Map (member)
+import qualified Data.Map.Lazy as Map (member, elems, delete)
 import Env
 import Helper
 import Test.Hspec
 import Types
 
 spec :: Spec
-spec =
+spec = do
   describe "Env.eval" $ do
     
     it "should return empty string when empty command is given" $
@@ -19,3 +19,8 @@ spec =
 
     it "user name should start with '@'" $
       evalState (eval "john flies away") emptyEnv `shouldBe` "Command john not found.\n"
+
+  describe "Env.findUsers" $
+
+    it "should return a Map containing the users found" $
+      findUsers ["foo", "bob", "barb"] stubUsers `shouldBe` Map.delete "alice" stubUsers
